ca37c42abd156774fae31da794cf44cc5ec22dce
Pheerapht <6306021632032@fitm.kmutnb.ac.th>
2023-12-22 18:06:49 +0700

    first commit

---
+++ lang/th/user_validation.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'About is required' => 'ระบุเกี่ยวกับ',
+    'Account field is required' => 'ต้องระบุข้อมูลในฟิลด์บัญชี',
+];
---
+++ lang/th/validation.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'accepted' => ':attribute ต้องได้รับการยอมรับ',
+    'accepted_if' => ':attribute จะต้องได้รับการยอมรับเมื่อ :other คือ :value',
+    'active_url' => ':attribute ไม่ใช่ URL ที่ถูกต้อง',
+];
