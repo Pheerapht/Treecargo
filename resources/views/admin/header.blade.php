94b6d3a6f036f5533fa99c22f6d14a9f54630e2d
preem2019 <157298788+preem2019@users.noreply.github.com>
2024-02-15 16:15:20 +0700

    admin-commit

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -6,6 +6,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    
    <link rel="shortcut icon"  href="{{ asset($setting->favicon) }}"  type="image/x-icon">
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
